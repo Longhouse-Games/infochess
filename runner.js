9a3f7a8cb78d00b38339b3b8fb336b1ed4177d89
rgould <rwgould@gmail.com>
2012-10-06 23:51:02 -0400

    Add jasmine test runner for running with node

---
+++ runner.js
@@ -0,0 +1,27 @@
+var requirejs = require('requirejs');
+requirejs.config({
+  nodeRequire: require
+//  baseUrl: 'js'
+});
+
+//make define available globally like it is in the browser
+global.define = require('requirejs');
+
+//make jasmine available globally like it is in the browser
+// global.describe = require('./lib/jasmine-1.1.0.rc1/jasmine').describe;
+// global.it = require('./lib/jasmine-1.1.0.rc1/jasmine').it;
+// global.expect = require('./lib/jasmine-1.1.0.rc1/jasmine').expect;
+global.describe = require('./vendor/jasmine-1.2.0/jasmine').describe;
+global.it = require('./vendor/jasmine-1.2.0/jasmine').it;
+global.expect = require('./vendor/jasmine-1.2.0/jasmine').expect;
+global.beforeEach = require('./vendor/jasmine-1.2.0/jasmine').beforeEach;
+
+
+//bring in and list all the tests to be run
+requirejs(['./spec/lib/building_board.spec'], function(BuildingBoardSpec) {
+  console.log("BuildingBoardSpec: ", BuildingBoardSpec);
+  var jasmine = require('./vendor/jasmine-1.2.0/jasmine').jasmine;
+  var ConsoleJasmineReporter2 = require('./vendor/runner/consoleJasmineReporter2').ConsoleJasmineReporter;
+  jasmine.getEnv().addReporter(new ConsoleJasmineReporter2());
+  jasmine.getEnv().execute();
+});

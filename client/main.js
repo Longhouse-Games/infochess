b854699240bf8d4eaf8c51fc6e56f45feb531918
rgould <rwgould@gmail.com>
2012-10-07 13:27:50 -0400

    Remove old GC code. Hook up new BuildingBoard.

--- client/main.js
+++ client/main.js
@@ -5,7 +5,7 @@ requirejs.config({
   }
 });
 
-require(["lib/checkers", 'helpers'], function(checkers, helpers) {
+require(["lib/checkers", "lib/building_board", 'helpers'], function(checkers, BuildingBoardModule, helpers) {
 
   if (Array.prototype.forEach === undefined) {
     Array.prototype.forEach = function(callback) {
@@ -15,33 +15,30 @@ require(["lib/checkers", 'helpers'], function(checkers, helpers) {
     };
   }
 
-  function Piece(colour, type, position) {
-    // TODO move this to /lib/
-    // TODO validate position
-    this.position = position;
-    // TODO validate colour
-    this.colour = colour;
-    // TODO validate type
-    this.type = type;
-  }
+  var BuildingBoard = BuildingBoardModule.BuildingBoard;
+  var Piece = BuildingBoardModule.Piece;
+  var Position = BuildingBoardModule.Position;
+
+  var TYPES = [
+    'king',
+    'queen',
+    'rook',
+    'knight',
+    'bishop',
+    'pawn'
+  ];
 
   var socket = io.connect();
   var g_role = 'spectator';
   var g_gameState = null;
+  var g_building_board = null;
   var g_selectedType; // Selected piece type when building army
 
-  function getPositionKey(position) {
-    if (!position) {
-      return undefined;
-    }
-    return position.x + "," + position.y;
-  }
-
-  function isSoldierPlayer() {
+  function isBlackPlayer() {
     return g_role === BLACK_ROLE;
   }
 
-  function isGuerrillaPlayer() {
+  function isWhitePlayer() {
     return g_role === WHITE_ROLE;
   }
 
@@ -54,253 +51,58 @@ require(["lib/checkers", 'helpers'], function(checkers, helpers) {
   }
 
   var g_piecesOnBoard = {}; // "x,y" -> div
-  var g_soldierPiecesOnBoard = {}; // "x,y" -> img
-  var g_guerrillaPiecesOnBoard = {}; // "x,y" -> img
 
-  var g_chessBoard = {}; // "x,y" -> Piece
 
   var SPECTATOR_ROLE = 'spectator';
   var WHITE_ROLE = 'white';
   var BLACK_ROLE = 'black';
   var SQUARE_SIZE = 70;
-  var SOLDIER_MARGIN = 39;
-  var GUERRILLA_MARGIN = 88;
+  var PIECE_MARGIN = 39;
 
-  function Army() {
-    return {
-      king: 0,
-      queen: 0,
-      knight: 0,
-      rook: 0,
-      bishop: 0,
-      pawn: 0
-    };
-  }
-
-  function ArmyLimits() {
-    return {
-      king: 1,
-      queen: 1,
-      knight: 2,
-      rook: 2,
-      bishop: 2,
-      pawn: 8
-    };
-  }
-
-  function getArmy() {
-    var army = new Army();
-
-    for (var key in g_chessBoard) {
-      if (g_chessBoard.hasOwnProperty(key)) {
-        var piece = g_chessBoard[key];
-        army[piece.type] += 1;
-      }
+  function getBuildingBoard() {
+    if (!g_building_board) {
+      g_building_board = new BuildingBoard();
     }
-    return army;
-  }
-
-  function calculatePoints(army) {
-    var points = army.queen * 3 +
-      (army.knight + army.rook) * 2 +
-      (army.bishop + army.pawn);
-    return points;
+    return g_building_board;
   }
 
   function recalculateArmy() {
     // TODO if game has started, die/throw/or something
-    var army = getArmy();
-    var points = calculatePoints(army);
+    var building_board = getBuildingBoard();
+    var points = building_board.points();
 
-    $("#points_remaining #points").text(10-points);
-    for (var type in army) {
-      if (army.hasOwnProperty(type)) {
-        $("#" + type + " .count").text(army[type]);
-      }
-    }
+    $("#points_remaining #points").text(building_board.max_points - points);
+    TYPES.forEach(function(type) {
+      $("#" + type + " .count").text(building_board.count(type));
+    });
   }
 
-  function pieceAt(position) {
-    var key = position.x+","+position.y;
-    var r = g_piecesOnBoard[key];
-    return r;
-  }
-
-  function addPiece(container, piece, className, margin, piecesOnBoard) {
+  function addPiece(container, piece, position, className, margin) {
     var newPieceOnBoard = document.createElement("div");
     newPieceOnBoard.className += " " + className;
-    newPieceOnBoard.style.left = margin + ((piece.position.x) * SQUARE_SIZE) + 'px';
-    newPieceOnBoard.style.bottom = margin + ((piece.position.y) * SQUARE_SIZE) + 'px';
+    newPieceOnBoard.style.left = margin + ((position.x) * SQUARE_SIZE) + 'px';
+    newPieceOnBoard.style.bottom = margin + ((position.y) * SQUARE_SIZE) + 'px';
     container.appendChild(newPieceOnBoard);
-    if (piecesOnBoard) {
-      piecesOnBoard[getPositionKey(piece.position)] = newPieceOnBoard;
-    }
     return newPieceOnBoard;
   }
 
-  function addTempPiece(piece) {
+  function addTempPiece(piece, position) {
     var container = document.getElementById('pieces');
-    var piecesOnBoard = g_piecesOnBoard;
     var cssclass = cssClassForPiece(piece) + " temp_piece";
-    var newPieceOnBoard = addPiece(container, piece, cssclass, SOLDIER_MARGIN, piecesOnBoard);
-    // TODO add mechanism to remove this piece from the board - x in corner or drag away
+    var newPieceOnBoard = addPiece(container, piece, position, cssclass, PIECE_MARGIN);
 
     //Add removal marker
     var removalMarker = document.createElement("div");
     removalMarker.className = "removal_marker";
     removalMarker.onclick = function() {
       container.removeChild(newPieceOnBoard);
-      delete piecesOnBoard[getPositionKey(piece.position)];
-      delete g_chessBoard[getPositionKey(piece.position)];
+      getBuildingBoard().removePiece(position);
       displayValidStartingPositions(getPlayerColour(), g_selectedType);
       recalculateArmy();
     };
     newPieceOnBoard.appendChild(removalMarker);
 
     return newPieceOnBoard;
-  }
-
-  function addSoldierPiece(piece, container) {
-    container = container || document.getElementById('pieces');
-    var piecesOnBoard = g_soldierPiecesOnBoard;
-    var newPieceOnBoard = addPiece(container, piece, 'soldier_piece', SOLDIER_MARGIN, piecesOnBoard);
-    addSoldierPieceBehaviour(piece);
-    return newPieceOnBoard;
-  }
-
-  var g_selectedSoldierPiece = null;
-  function getSelectedSoldierPiece() {
-    return g_selectedSoldierPiece;
-  }
-
-  function setSelectedSoldierPiece(piece) {
-    var positionKey = null;
-    for (positionKey in g_soldierPiecesOnBoard) {
-      var otherPieceOnBoard = g_soldierPiecesOnBoard[positionKey];
-      var className = otherPieceOnBoard.className.replace(/\s*selected/g, '');
-      otherPieceOnBoard.className = className;
-    }
-    if (g_gameState.isSoldierTurn() && piece) {
-      positionKey = getPositionKey(piece.position);
-      var pieceOnBoard = g_soldierPiecesOnBoard[positionKey];
-      g_selectedSoldierPiece = pieceOnBoard;
-      if (pieceOnBoard) {
-        pieceOnBoard.className += " selected";
-      }
-    } else {
-      g_selectedSoldierPiece = null;
-    }
-    if (piece) {
-      updateSoldierMoves(piece);
-    } else {
-      hideSoldierMoves();
-    }
-  }
-
-  function addSoldierPieceBehaviour(piece) {
-    var positionKey = getPositionKey(piece.position);
-    var pieceOnBoard = g_soldierPiecesOnBoard[positionKey];
-    if (!pieceOnBoard) {
-      return;
-    }
-    if (isSoldierPlayer()) {
-      pieceOnBoard.onclick = function() {
-        if (!g_gameState.movedSoldier) {
-          setSelectedSoldierPiece(piece);
-        }
-      };
-    }
-  }
-
-  function addGuerrillaPiece(piece, container) {
-    container = container || document.getElementById('pieces');
-    var piecesOnBoard = g_guerrillaPiecesOnBoard;
-    var newPieceOnBoard = addPiece(container, piece, 'guerrilla_piece', GUERRILLA_MARGIN, piecesOnBoard);
-    return newPieceOnBoard;
-  }
-
-  function updatePieces(arrPieces, piecesOnBoard, addPiece) {
-    arrPieces = arrPieces || [];
-    piecesOnBoard = piecesOnBoard || {};
-    var removedPieces = {};
-    var positionKey;
-    var pieceOnBoard;
-    for (positionKey in piecesOnBoard) {
-      removedPieces[positionKey] = piecesOnBoard[positionKey];
-    }
-    // add new pieces
-    for (var idxPiece = 0; idxPiece < arrPieces.length; ++idxPiece) {
-      var piece = arrPieces[idxPiece];
-      positionKey = getPositionKey(piece.position);
-      pieceOnBoard = piecesOnBoard[positionKey];
-      if (!pieceOnBoard) {
-        addPiece(piece);
-      }
-      delete removedPieces[positionKey];
-    }
-    // remove extra pieces
-    for (positionKey in removedPieces) {
-      delete piecesOnBoard[positionKey];
-      pieceOnBoard = removedPieces[positionKey];
-      var parentNode = pieceOnBoard.parentNode;
-      if (parentNode) {
-        parentNode.removeChild(pieceOnBoard);
-      }
-    }
-  }
-
-  function updateSoldierPieces() {
-    if (g_gameState) {
-      var arrPieces = g_gameState.arrSoldierPieces;
-      updatePieces(arrPieces, g_soldierPiecesOnBoard, addSoldierPiece);
-      if (isSoldierPlayer() && g_gameState.movedSoldier) {
-        setSelectedSoldierPiece(g_gameState.movedSoldier);
-      }
-    }
-  }
-
-  function updateGuerrillaPieces() {
-    if (g_gameState) {
-      var arrPieces = g_gameState.arrGuerrillaPieces;
-      updatePieces(arrPieces, g_guerrillaPiecesOnBoard, addGuerrillaPiece);
-    }
-  }
-
-  function createGuerrillaMove($moves, move) {
-    var piece = { position: move };
-    var container = $moves.get(0);
-    var newPieceOnBoard = addPiece(container, piece, 'guerrilla_piece', GUERRILLA_MARGIN);
-    newPieceOnBoard.onclick = function() {
-      socket.emit('placeGuerrilla', piece);
-    };
-  }
-
-  function hideGuerrillaMoves() {
-    var $moves = $('#guerrilla_moves');
-    $moves.css('visibility', 'hidden');
-  }
-
-  function getPieceCost(type) {
-    var cost = null;
-    switch(type) {
-      case 'king':
-        cost = 0;
-        break;
-      case 'queen':
-        cost = 3;
-        break;
-      case 'knight':
-      case 'rook':
-        cost = 2;
-        break;
-      case 'bishop':
-      case 'pawn':
-        cost = 1;
-        break;
-      default:
-        throw new Error("Invalid type: '"+type+"'");
-    }
-    return cost;
   }
 
   function displayValidStartingPositions(side, piece_type) {
@@ -311,95 +113,18 @@ require(["lib/checkers", 'helpers'], function(checkers, helpers) {
     $moves.text("");
 
     // Determine if placement of this piece would go over the army limit
-    var army = getArmy();
-    var total = calculatePoints(army);
-    var piece_cost = getPieceCost(piece_type);
-    if ((total + piece_cost) > 10) {
+    var building_board = getBuildingBoard();
+    var piece = new Piece(piece_type, getPlayerColour());
+    var positions = building_board.getPossiblePlacements(piece);
+    if (positions.length === 0) {
       return;
     }
 
-    // Determine if the limits for thise type have been reached
-    var armyLimit = new ArmyLimits();
-    if ((army[piece_type] + 1) > armyLimit[piece_type]) {
-      return;
-    }
-
-    // Determine all possible positions
-    var positions = [];
-    var i, pos;
-    var row = getPlayerColour() === 'white' ? 0 : 7;
-    if (piece_type === 'king' || piece_type === 'rook' || piece_type === 'queen' || piece_type === 'knight') {
-      // back row
-      for (i = 0; i < 8; i++) {
-        positions.push({ x: i, y: row });
-      }
-    } else if (piece_type === 'bishop') {
-      var white = null;
-      var black = null;
-      // Search for black and white pieces first
-      for (i = 0; i < 8; i++) {
-        pos = { x: i, y: row };
-        if (pieceAt(pos) && g_chessBoard[getPositionKey(pos)].type === 'bishop') {
-          if ((i+row) % 2 === 0) {
-            white = pos;
-          } else {
-            black = pos;
-          }
-        }
-      }
-      // back row
-      for (i = 0; i < 8; i++) {
-        pos = { x: i, y: row };
-        if (!pieceAt(pos)) {
-          if (!(white && ((i+row) % 2 === 0)) && !(black && ((i+row) % 2 === 1))) {
-            positions.push(pos);
-          }
-        }
-      }
-    } else if (piece_type === 'pawn') {
-      // second from back row
-      for (i = 0; i < 8; i++) {
-        row = getPlayerColour() === 'white' ? 1 : 6;
-        positions.push({ x: i, y: row });
-      }
-    } else {
-      alert("Error encountered: invalid piece_type '"+piece_type+"'. Try refreshing the page.");
-      return;
-    }
-
-    // Display shadow pieces on unoccpied squares
     for (i = 0; i < positions.length; i++) {
       var position = positions[i];
-      if (!pieceAt(position)) {
-        piece = new Piece(getPlayerColour(), piece_type, position);
-        createMove($moves, piece, position);
-      }
+      createMove($moves, piece, position);
     }
     $moves.css('visibility', 'visible');
-  }
-
-  function showGuerrillaMoves() {
-    var $moves = $('#guerrilla_moves');
-    $moves.text("");
-    var arrMoves = g_gameState.getPotentialGuerrillaMoves();
-    for (var idx = 0; idx < arrMoves.length; ++idx) {
-      var move = arrMoves[idx];
-      createGuerrillaMove($moves, move);
-    }
-    $moves.css('visibility', 'visible');
-  }
-
-  function updateGuerrillaMoves() {
-    if (!g_gameState) {
-      return;
-    }
-    hideGuerrillaMoves();
-    if (!isGuerrillaPlayer()) {
-      return;
-    }
-    if (g_gameState.isGuerrillaTurn()) {
-      showGuerrillaMoves();
-    }
   }
 
   function cssClassForPiece(piece) {
@@ -407,62 +132,16 @@ require(["lib/checkers", 'helpers'], function(checkers, helpers) {
   }
 
   function createMove($moves, piece, position) {
-    var move = { piece: piece, position: position };
     var container = $moves.get(0);
     var cssclass = "shadow_piece " + cssClassForPiece(piece);
-    var newPieceOnBoard = addPiece(container, move, cssclass, SOLDIER_MARGIN);
+    var newPieceOnBoard = addPiece(container, piece, position, cssclass, PIECE_MARGIN);
     newPieceOnBoard.onclick = function() {
-      console.log("Adding " + move.piece.type + " to position '"+move.position.x+","+move.position.y+"'");
-      addTempPiece(piece);
-      g_chessBoard[position.x+","+position.y] = piece;
+      console.log("Adding " + piece.type + " to position '"+position.asKey()+"'");
+      addTempPiece(piece, position);
+      getBuildingBoard().addPiece(piece, position);
       recalculateArmy();
       displayValidStartingPositions(getPlayerColour(), g_selectedType);
     };
-  }
-
-  function createSoldierMove($moves, piece, position) {
-    var move = { piece: piece.position, position: position };
-    var container = $moves.get(0);
-    var newPieceOnBoard = addPiece(container, move, 'soldier_piece', SOLDIER_MARGIN);
-    newPieceOnBoard.onclick = function() {
-      socket.emit('moveCOIN', move);
-      setSelectedSoldierPiece(null);
-      hideSoldierMoves();
-    };
-  }
-
-  function hideSoldierMoves() {
-    var $moves = $('#soldier_moves');
-    $moves.css('visibility', 'hidden');
-  }
-
-  function showSoldierMoves(piece) {
-    var $moves = $('#soldier_moves');
-    $moves.text("");
-    var arrMoves;
-    if (g_gameState.movedSoldier) {
-      arrMoves = g_gameState.getSoldierCapturingMoves(piece);
-    } else {
-      arrMoves = g_gameState.getPotentialSoldierMoves(piece);
-    }
-    for (var idx = 0; idx < arrMoves.length; ++idx) {
-      var position = arrMoves[idx];
-      createSoldierMove($moves, piece, position);
-    }
-    $moves.css('visibility', 'visible');
-  }
-
-  function updateSoldierMoves(piece) {
-    if (!g_gameState) {
-      return;
-    }
-    hideSoldierMoves();
-    if (!isSoldierPlayer()) {
-      return;
-    }
-    if (piece && g_gameState.isSoldierTurn()) {
-      showSoldierMoves(piece);
-    }
   }
 
   function setTransitionProperty($element, value) {
@@ -533,12 +212,12 @@ require(["lib/checkers", 'helpers'], function(checkers, helpers) {
       setOverlayText($overlay, g_gameState.getCurrentPhase() + "'S TURN");
       return;
     }
-    if (isSoldierPlayer()) {
-      setOverlayText($overlay, g_gameState.isSoldierTurn() ? yourTurn : opponentsTurn);
+    if (isWhitePlayer()) {
+      setOverlayText($overlay, g_gameState.isGuerrillaTurn() ? yourTurn : opponentsTurn);
       return;
     }
-    if (isGuerrillaPlayer()) {
-      setOverlayText($overlay, g_gameState.isGuerrillaTurn() ? yourTurn : opponentsTurn);
+    if (isBlackPlayer()) {
+      setOverlayText($overlay, g_gameState.isSoldierTurn() ? yourTurn : opponentsTurn);
       return;
     }
   }
@@ -677,9 +356,6 @@ require(["lib/checkers", 'helpers'], function(checkers, helpers) {
 
       updateArmySelector();
       updatePlayerTurnOverlay();
-      updateGuerrillaPieces();
-      updateSoldierPieces();
-      updateGuerrillaMoves();
     });
 
     // send message functionality

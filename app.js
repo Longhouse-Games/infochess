Longhouse-Games/guerrilla-checkers

Upgrade to node 0.10.
Remove dead code.
Wrap local dependencies with requirejs define()s

app.js
@@ -1,19 +1,37 @@
-var Raven = require('raven');
+var requirejs = require('requirejs');
 
-var GuerrillaCheckers = require('./server/server.js');
-gc = Raven.init(GuerrillaCheckers);
-
-gc.configure({
-
-  send_index: function(request, response) {
-    response.sendfile(__dirname + '/index.html');
+requirejs.config({
+  baseUrl: __dirname,
+  nodeRequire: require,
+  paths: {
+    lib: "./lib",
+    server: "./server",
+    underscore: "./vendor/underscore"
   },
-
-  send_asset: function(request, response, path) {
-    path = __dirname + path;
-    response.sendfile(path);
+  shim: {
+    underscore: {
+      exports: '_'
+    }
   }
-
 });
 
-gc.run();
+Raven = require('raven');
+
+requirejs(['./server/server'], function(GuerrillaCheckers) {
+
+  gc = Raven.init(GuerrillaCheckers);
+  gc.configure({
+
+    send_index: function(request, response) {
+      response.sendfile('/index.html', { root: __dirname });
+    },
+
+    send_asset: function(request, response, path) {
+      response.sendfile(path, { root: __dirname });
+    }
+
+  });
+
+  gc.run();
+
+});

Longhouse-Games/infochess

Conflicts:
	app.js
	package.json
	server/egs_notifier.js
	server/server.js

app.js
@@ -1,16 +1,35 @@
-var Raven = require('raven');
+var requirejs = require('requirejs');
 
-var InfoChess = require('./server/server');
-ic = Raven.init(InfoChess);
-
-ic.configure({
-  send_index: function(request, response) {
-    response.sendfile(__dirname + "/index.html");
+requirejs.config({
+  baseUrl: __dirname,
+  nodeRequire: require,
+  paths: {
+    lib: "./lib",
+    server: "./server",
+    underscore: "./vendor/underscore"
   },
-  send_asset: function(request, response, path) {
-    path = __dirname + path;
-    response.sendfile(path);
+  shim: {
+    underscore: {
+      exports: '_'
+    }
   }
 });
 
-ic.run();
+Raven = require('raven');
+
+requirejs(['./server/server'], function(InfoChess) {
+
+  ic = Raven.init(InfoChess);
+
+  ic.configure({
+    send_index: function(request, response) {
+      response.sendfile('/index.html', { root: __dirname });
+    },
+    send_asset: function(request, response, path) {
+      response.sendfile(path, { root: __dirname });
+    }
+  });
+
+  ic.run();
+
+});